37fbcd502f98ba9c8988565220414509e9b4366d
Devork <88288729+Devorkk@users.noreply.github.com>
2023-01-22 15:17:41 +0100

    Initial Commit

---
+++ source/user-scripts/AxisRotation.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name                 KoGaMa Axis Rotation
+// @version              0.0.0
+// @author               Devork
+// @namespace            https://github.com/Devorkk/
+// @match                https://www.kogama.com/build/*
+// @match                https://kogama.com.br/build/*
+// @match                https://friends.kogama.com/build/*
+// @match                https://www.kogama.com/page/webgl-frame/*
+// @match                https://kogama.com.br/page/webgl-frame/*
+// @match                https://friends.kogama.com/page/webgl-frame/*
+// @require              https://unpkg.com/guify@0.12.0/lib/guify.min.js
+// @require              https://raw.githubusercontent.com/Devorkk/AxisRotation/main/source/scripts/AxisRotationSource.js
+// @run-at               document-start
+// @grant                none
+// ==/UserScript==
